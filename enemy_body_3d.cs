a69e3f3c437cb8f1bc1fbfd77b4dacbe0e5b030f
Manne <manne@mosslightstudio.com>
2026-04-24 12:09:48 +0200

    working on navigation meshes

---
+++ enemy_body_3d.cs
@@ -0,0 +1,30 @@
+using Godot;
+using System;
+
+public partial class enemy_body_3d : CharacterBody3D
+{
+	[Export] public float Speed = 5.0f;
+	[Export] public NodePath PlayerPath;
+
+	CharacterBody3D player;
+	NavigationAgent3D nav_agent;
+
+	public override void _Ready()
+	{
+		player = GetNode<CharacterBody3D>(PlayerPath);
+		nav_agent = GetNode<NavigationAgent3D>("NavigationAgent3D");
+	}
+
+	public override void _PhysicsProcess(double delta)
+	{
+		Vector3 velocity = Velocity;
+
+
+		nav_agent.SetTargetPosition(player.GlobalPosition);
+		Vector3 nav_point_next = nav_agent.GetNextPathPosition();
+		velocity = (nav_point_next - GlobalPosition).Normalized() * Speed;
+
+		Velocity = velocity;
+		MoveAndSlide();
+	}
+}
--- player_controller.cs
+++ player_controller.cs
@@ -3,35 +3,35 @@ using System;
 
 public partial class player_controller : CharacterBody3D
 {
-	public const float Base_Speed = 5.0f;
-	public const float JumpVelocity = 4.5f;
-    [Export] public float Sensitivity = 0.1f;
-    Node3D Head;
+	[Export] public float Base_Speed = 5.0f;
+	[Export] public float JumpVelocity = 4.5f;
+	[Export] public float Sensitivity = 0.1f;
+	Node3D Head;
 
-    public override void _Ready()
-    {
-        Input.MouseMode = Godot.Input.MouseModeEnum.Captured;
-        Head = GetNode<Node3D>("Head3D");
-    }
+	public override void _Ready()
+	{
+		Input.MouseMode = Godot.Input.MouseModeEnum.Captured;
+		Head = GetNode<Node3D>("Head3D");
+	}
 
-    public override void _UnhandledInput(InputEvent @event)
-    {
-        if(@event is InputEventMouseMotion motion)
-        {
-            RotateY(-motion.Relative.X * Sensitivity);
-            float head_rotation = -motion.Relative.Y * Sensitivity;
-            float rotation_new = Head.Rotation.X + head_rotation;
-            if(rotation_new <= 1.5f && rotation_new >= -1.5f)
-            {
-                Head.RotateX(head_rotation); // input here is relative
-            }
-        }
-    }
+	public override void _UnhandledInput(InputEvent @event)
+	{
+		if(@event is InputEventMouseMotion motion)
+		{
+			RotateY(-motion.Relative.X * Sensitivity);
+			float head_rotation = -motion.Relative.Y * Sensitivity;
+			float rotation_new = Head.Rotation.X + head_rotation;
+			if(rotation_new <= 1.5f && rotation_new >= -1.5f)
+			{
+				Head.RotateX(head_rotation); // input here is relative
+			}
+		}
+	}
 
 	public override void _PhysicsProcess(double delta)
 	{
 		Vector3 velocity = Velocity;
-        float speed = Base_Speed;
+		float speed = Base_Speed;
 
 
 		// Add the gravity.
@@ -48,11 +48,10 @@ public partial class player_controller : CharacterBody3D
 
 		if (Input.IsActionPressed("Move_Sprint"))
 		{
-            speed *= 1.5f;
+			speed *= 1.5f;
 		}
 
 		// Get the input direction and handle the movement/deceleration.
-		// As good practice, you should replace UI actions with custom gameplay actions.
 		Vector2 inputDir = Input.GetVector("Move_Left", "Move_Right", "Move_Forward", "Move_Backward");
 		Vector3 direction = (Transform.Basis * new Vector3(inputDir.X, 0, inputDir.Y)).Normalized();
 		if (direction != Vector3.Zero)
