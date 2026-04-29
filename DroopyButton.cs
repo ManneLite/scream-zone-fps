366f5fc60ef7ecb0a9956fddd4c60253774f23cc
Manne <manne@mosslightstudio.com>
2026-04-29 22:09:07 +0200

    temp

---
+++ DroopyButton.cs
@@ -0,0 +1,16 @@
+using Godot;
+using System;
+
+public partial class DroopyButton : Godot.Button
+{
+    private AudioStreamPlayer sound;
+    
+    public override void _Ready()
+    {
+        
+    }
+    
+    public override void _Pressed()
+    {
+    }
+}
