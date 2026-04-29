a36fa4042bc325d0f479ace38cc91a6b66813123
Manne <manne@mosslightstudio.com>
2026-04-29 22:43:06 +0200

    spook

--- MenuMain.cs
+++ MenuMain.cs
@@ -6,14 +6,37 @@ public partial class MenuMain : Control
 
     Control CreditsScreen;
     Control MenuButtons;
+    Control Spooky_JPG;
+    Control normal_menu;
+    AudioStreamPlayer sound;
+    bool been_jumpscared = false;
 
     override public void _Ready()
     {
+        normal_menu = GetNode<Control>("Book");
+        Spooky_JPG = GetNode<Control>("Spooky_JPG");
         CreditsScreen = GetNode<Control>("Book/CreditsScreen");
         MenuButtons = GetNode<Control>("Book/MenuButtons");
+        sound = GetNode<AudioStreamPlayer>("Spooky_JPG/AudioStreamPlayer");
+        sound.Finished += () => {Spooky_JPG.Hide(); normal_menu.Show();};
 
+        Spooky_JPG.Hide();
         CreditsScreen.Hide();
         MenuButtons.Show();
+
+
+    }
+
+    public void OnTitlePressed()
+    {
+        if(!been_jumpscared)
+        {
+            sound.Play();
+            normal_menu.Hide();
+            Spooky_JPG.Show();
+        }
+
+        been_jumpscared = true;
     }
 
     public void OnPlayButtonPressed()
